364cd649108217a21c1dd0beac3b24fd714a086e
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-13 17:33:59 -0700

    tests passing for author and author detail page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,29 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { authors } = require('../data/authors-data');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/authors should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    const expected = authors.map((author) => {
+      return { id: author.id, name: author.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/authors/:id should return one author from the list', async () => {
+    const res = await request(app).get('/authors/1');
+    const toni = {
+      id: '1',
+      name: 'Toni Morrison',
+      dob: '1931-02-18T08:00:00.000Z',
+      pob: 'Lorain, OH',
+    };
+    expect(res.body).toEqual(toni);
   });
   afterAll(() => {
     pool.end();
