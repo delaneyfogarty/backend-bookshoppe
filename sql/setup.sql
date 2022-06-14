004880af13ad8115086a93b62711491bb2bd048e
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-13 17:40:59 -0700

    setup books table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP table if exists authors;
+DROP table if exists books;
 
 CREATE table authors (
 	id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -16,3 +17,21 @@ INSERT INTO authors (name, dob, pob) VALUES
 ('Margaret Atwood', '11/18/1939', 'Ottawa, Canada'),
 ('Zadie Smith', '10/25/1975', 'London, UK'),
 ('Angela Carter', '05/07/1940', 'Eastbourne, UK');
+
+CREATE table books (
+		id BIGINT GENERATED ALWAYS AS IDENTITY,
+		title VARCHAR NOT NULL,
+		year INT NOT NULL
+);
+
+INSERT INTO books (title, year) VALUES
+('Beloved', 1987),
+('Song of Solomon', 1977),
+('Half of a Yellow Sun', 2006),
+('Americanah', 2013),
+('The Testaments', 2019),
+('Swing Time', 2016),
+('NW', 2012),
+('The Bloody Chamber', 1979),
+('The Magic Toyshop', 1967),
+('Nights at the Circus', 1984);
